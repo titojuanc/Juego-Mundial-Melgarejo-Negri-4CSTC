3fcd704a42f24d0c5990f49249f92e72a2c9786d
alumno26.melgarejo.juan@ipm.edu.ar <alumno26.melgarejo.juan@ipm.edu.ar>
2026-05-04 17:48:42 -0300

    Base de evento creada. Falta trabajar la asignación de peligros

--- scripts/debug_encuentro.gd
+++ scripts/debug_encuentro.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var encuentro = preload("res://scenes/Encuentro.tscn")
+var evento = preload("res://eventos/alaska/rueda_pinchada.tres")
 @onready var auto:CharacterBody2D = $Auto
 @onready var fondo:Control = $Ruta_1
 var instancia_encuentro = null 
@@ -16,6 +17,7 @@ func _physics_process(delta: float) -> void:
 			fondo.parar() 
 			en_movimiento=false
 			instancia_encuentro = encuentro.instantiate()
+			instancia_encuentro.configurar_peligros(evento)
 			instancia_encuentro._iniciar(auto)
 			add_child(instancia_encuentro)
 		else:
