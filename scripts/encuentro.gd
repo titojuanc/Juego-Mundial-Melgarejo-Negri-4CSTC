e981655f3c76b40e66e6d240d5f3c878eb498d4c
alumno26.melgarejo.juan@ipm.edu.ar <alumno26.melgarejo.juan@ipm.edu.ar>
2026-05-05 11:49:05 -0300

    Aleatorización de peligros por evento realizada, falta implementar en la hotbar real

--- scripts/encuentro.gd
+++ scripts/encuentro.gd
@@ -10,8 +10,48 @@ func _iniciar(referencia_auto: CharacterBody2D) -> void:
 	add_child(barra_de_evento)
 
 func configurar_peligros(evento:Resource):
-	RandomNumberGenerator.randi()
+	var hotbar_peligros = ["", "", "", "", "", "", "", "" ]
+	var aleatorizador = RandomNumberGenerator.new()
+	var minimo_total =0
+	for minimo in evento.minimos:
+		minimo_total += evento.minimos[minimo]
 	
+	var maximo_total=0
+	for maximo in evento.maximos:
+		maximo_total+=evento.maximos[maximo]
+	
+	var claves = evento.peligros.keys()
+	
+	var conteo = {}
+	for clave in claves:
+		conteo[clave] = 0 
+	
+	var i = 0
+	while i < minimo_total:
+		var peligro = claves[aleatorizador.randi_range(0, 3)]
+		var chance = evento.peligros[peligro]
+		var roll = aleatorizador.randf_range(0.0, 100.0)
+		if roll < chance:
+			if conteo[peligro] < evento.maximos[peligro]:
+				print("apareció " + peligro)
+				hotbar_peligros[i] = peligro
+				conteo[peligro] += 1
+				i += 1
+			else:
+				print("maximo de "+peligro+" alcanzado.")
+		else:
+			print("no apareció " + peligro)
+	
+	for peligro in evento.minimos:
+		var faltan = evento.minimos[peligro] - conteo[peligro]
+		if faltan > 0:
+			for j in range(faltan):
+				hotbar_peligros[i] = peligro
+				conteo[peligro] += 1
+				i += 1
+	
+	print(hotbar_peligros)
+
 
 func terminar():
 	queue_free()
