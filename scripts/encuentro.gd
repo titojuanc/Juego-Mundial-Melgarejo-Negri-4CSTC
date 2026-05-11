0980b0b962a84ac82bf72097180ff7fc691b16d9
alumno26.melgarejo.juan@ipm.edu.ar <alumno26.melgarejo.juan@ipm.edu.ar>
2026-05-11 17:21:31 -0300

    Versión 1.0 del randomizador de eventos.

--- scripts/encuentro.gd
+++ scripts/encuentro.gd
@@ -27,7 +27,7 @@ func configurar_peligros(evento:Resource):
 	for clave in claves:
 		conteo[clave] = 0 
 	
-	var chances_extra = [50.0, 20.0, 10.0]
+	var chances_extra = [50.0, 25.0, 20.0]
 	var orden = [3, 4, 2, 5, 1, 6, 0, 7]
 	var cursor = 0
 	
@@ -35,7 +35,7 @@ func configurar_peligros(evento:Resource):
 	while i < minimo_total:
 		var pendientes = [] #aca se guardan los minimos
 		for clave in claves:
-			if conteo[clave] < evento.minimos[clave]:
+			if conteo[clave] < evento.minimos[clave] and evento.peligros[clave] != 0:
 				pendientes.append(clave)
 		
 		if pendientes.is_empty(): # si el evento es totalmente random, no hace nada
@@ -45,7 +45,7 @@ func configurar_peligros(evento:Resource):
 		var chance = evento.peligros[peligro]
 		var roll = aleatorizador.randf_range(0.0, 100.0)
 		if roll < chance:
-			if conteo[peligro] < evento.maximos[peligro]: #aca se controla los maximos
+			if conteo[peligro] < evento.maximos[peligro] or evento.maximos[peligro]==0: #aca se controla los maximos
 				hotbar_peligros[orden[cursor]] = peligro
 				conteo[peligro] += 1
 				i += 1
@@ -57,11 +57,25 @@ func configurar_peligros(evento:Resource):
 			for j in range(faltan):
 				hotbar_peligros[orden[cursor]] = peligro
 				conteo[peligro] += 1
-				i += 1
 				cursor +=1
 	
-	for chance_extra
-	
+	for chance_extra in chances_extra:
+		var disponibles = [] 
+		for clave in claves:
+			if conteo[clave] < evento.maximos[clave] and evento.peligros[clave] != 0:
+				disponibles.append(clave)
+		
+		if disponibles.is_empty(): #no hay mas peligros para poner
+			break
+		var peligro =  disponibles[aleatorizador.randi_range(0, disponibles.size()-1)]
+		var roll = aleatorizador.randf_range(0.0, 100.0)
+		if roll < chance_extra:
+			if conteo[peligro] < evento.maximos[peligro] or evento.maximos[peligro]==0: #aca se controla los maximos
+				hotbar_peligros[orden[cursor]] = peligro
+				conteo[peligro] += 1
+				cursor +=1
+				
+			
 	print(hotbar_peligros)
 
 func terminar():
